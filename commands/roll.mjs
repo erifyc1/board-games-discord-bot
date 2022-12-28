a3d03806df2e97a07094e4b19beaf20c34ffb641
Ryan Ziegler <rzig408@gmail.com>
2022-12-27 19:45:06 -0600

    update command

--- commands/roll.mjs
+++ commands/roll.mjs
@@ -1,18 +1,38 @@
-import { SlashCommandBuilder } from '@discordjs/builders';
+import { SlashCommandBuilder } from "@discordjs/builders";
+import evalroll from "../lib/evalroll.mjs";
 
 export const data = new SlashCommandBuilder()
-    .setName('roll')
-    .setDescription('Rolls dice')
-    .addStringOption(option =>
-		option.setName('dice')
-			.setDescription('Enter a dice roll (ex: 2d6)')
-			.setRequired(true)
-			);
-    
+  .setName("roll")
+  .setDescription("Rolls dice")
+  .addStringOption((option) =>
+    option
+      .setName("dice")
+      .setDescription("Enter a dice roll (ex: 2d6)")
+      .setRequired(true)
+  );
+
+function prettify(evalOutput) {
+  const rollsNew = evalOutput.value.postDiscard.join(", ");
+  const sumNew = evalOutput.value.postDiscard.reduce((a, b) => a + b);
+  const rollsOld = evalOutput.value.preDiscard.join(", ");
+  const sumOld = evalOutput.value.preDiscard.reduce((a, b) => a + b);
+  let same = rollsNew.length === rollsOld.length;
+  if (!same) {
+    return `Rolls: ${rollsNew} (sum ${sumNew}). Old rolls were: ${rollsOld} (sum ${sumOld})`;
+  } else {
+    return `Rolls: ${rollsNew} (sum ${sumNew})`;
+  }
+}
+
 export async function execute(interaction) {
-    const dice = await interaction.options.getString('dice');
-    /**
-     * @todo parse dice
-     */
-    await interaction.reply({ content: dice ? dice : 'empty', ephemeral: true });
-};
+  const dice = await interaction.options.getString("dice");
+  /**
+   * @todo parse dice
+   */
+  await interaction.reply({
+    content: dice
+      ? `${prettify(evalroll(dice))}. Your command was ${dice}.`
+      : "empty",
+    ephemeral: true,
+  });
+}
