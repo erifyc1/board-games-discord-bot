3ee111a50f9ff3fc35d37376e5323dfe9bf63e76
Jacob Stolker <stolkerja@gmail.com>
2023-03-23 18:14:17 -0500

    add support for image banners and added directions to the disclosed location

--- commands/event.mjs
+++ commands/event.mjs
@@ -1,10 +1,27 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
 import client from "../client.mjs";
 
-const GAMES = ["Two Rooms and a Boom", "Blood on the Clocktower"];
+const GAMES = [
+	{
+		name: "Two Rooms and a Boom",
+		imgpath: "./images/traab.jpg"
+	}, 
+	{
+		name: "Blood on the Clocktower",
+		imgpath: "./images/botct.jpg"
+	}
+];
 const BOT_NAME = "BGB";
 const BOT_DISC = "5311";
 const EVENT_NAME = "Board Games";
+const DEFAULT_IMGPATH = "./images/default-bg.png";
+const directions = "\n\n\n\n\n\n\n\n\n \
+**Directions:**\n \
+1.Come to Octave Apartments\n \
+\t(210 S 4th St, Champaign, IL 61820)\n \
+2.Go to building 2 (the one with the garage)\n \
+3.Head up to the 3rd floor\n \
+4.Find your way to the large study room (aka the disclosed location)";
 
 function nextAt(day, time, is_am) {
   let now = new Date();
@@ -50,7 +67,7 @@ export const data = new SlashCommandBuilder()
           .setName("game")
           .setDescription("Choose the game we'll play")
           .setRequired(true)
-          .setChoices(...GAMES.map((game) => ({ name: game, value: game })), {
+          .setChoices(...GAMES.map((game) => ({ name: game.name, value: game.name })), {
             name: "TBD",
             value: "default",
           })
@@ -99,9 +116,11 @@ export async function execute(interaction) {
     const nextEvent = await getNextBoardGamesEventIfExists(guild);
     const gameChoice = interaction.options.getString("game");
     let eventDescription =
-      "We aren't sure what we will be playing yet, but will update this event when we decide!";
+      `We aren't sure what we will be playing yet, but will update this event when we decide! ${directions}`;
+		let eventImgPath = DEFAULT_IMGPATH;
     if (gameChoice != "default") {
-      eventDescription = `Join us for ${gameChoice}!`;
+      eventDescription = `Join us for ${gameChoice}! ${directions}`;
+			eventImgPath = GAMES.find((game) => { return game.name == gameChoice }).imgpath;
     }
     const newMetadata = {
       name: EVENT_NAME,
@@ -113,6 +132,7 @@ export async function execute(interaction) {
       entityMetadata: {
         location: "Disclosed Location",
       },
+			image: eventImgPath,
     };
     if (nextEvent) {
       await guild.scheduledEvents.edit(nextEvent, newMetadata);
